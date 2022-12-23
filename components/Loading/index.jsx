1ac661dd12775ecb58d1eac965b55347b25f7152
durbonca <durbonca@gmail.com>
2022-12-23 18:53:18 -0300

    single quotes

--- components/Loading/index.jsx
+++ components/Loading/index.jsx
@@ -1,7 +1,7 @@
-import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import { ArrowPathIcon } from '@heroicons/react/24/outline'
 
 export const Loading = () => {
   return (
-    <ArrowPathIcon className="animate-spin w-16 h-16 mx-2.5 text-blue-500" />
-  );
-};
+    <ArrowPathIcon className='animate-spin w-16 h-16 mx-2.5 text-blue-500' />
+  )
+}
--- components/Social/Logo.jsx
+++ components/Social/Logo.jsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export const Logo = ({ href, logo, alt, className }) => {
   return (
     <a
       href={href}
-      className="flex items-center w-16 h-16 mx-2.5"
-      target="_blank"
-      rel="noreferrer"
+      className='flex items-center w-16 h-16 mx-2.5'
+      target='_blank'
+      rel='noreferrer'
     >
       <Image className={className} src={logo} alt={alt} />
     </a>
-  );
-};
+  )
+}
